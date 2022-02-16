84031a67b3a270cc1f52fb024506d748ee64d8e4
Labonnya <75426550+Labonnya@users.noreply.github.com>
2022-02-16 23:17:40 +0600

    Create registration.cpp

---
+++ registration.cpp
@@ -0,0 +1,17 @@
+void registration()
+{
+    string name,email_id,password,user_name;
+    int phone_no,date_of_birth;
+    cout<<"Fill up the following informations to sign up with us today.";
+    cout<<"\n";
+    cout<<"Enter your name: ";
+    cin>>name;
+    int count=0;
+    for(int i=0;name[i]!='\0';i++){
+      count++;
+      if(name[i]!=)
+
+   }
+
+
+}
