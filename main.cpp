b6ddf13a3c5dfd0d54bc781c60ba93db79e1ad0d
Labonnya <75426550+Labonnya@users.noreply.github.com>
2022-02-16 23:17:09 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+    int choice;
+    cout<<"\t\tWELCOME TO FLYFILER";
+    cout<<"\n";
+    cout<<"Do you want to sign up or sign in? Press 1 for sign up and press 2 for sign in.";
+    cin>>choice;
+    if(choice==1)
+    {
+        registration();
+    }
+}
